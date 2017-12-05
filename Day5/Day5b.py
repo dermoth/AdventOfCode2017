d7a8783983a4e6ffcc55c35d569af096521b0fd2
dermoth <dermoth@r830>
2017-12-05 00:33:16 -0500

    Day5

---
+++ Day5/Day5b.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+data = []
+with open('input.txt', 'r') as f:
+    data.extend([int(i) for i in f])
+
+#data = [0, 3, 0, 1, -3]
+
+i = 0
+count = 0
+while True:
+    count += 1
+    step = data[i]
+    if step >= 3:
+        data[i] -= 1
+    else:
+        data[i] += 1
+    i += step
+    if i >= len(data):
+        break
+
+print(count)
+
